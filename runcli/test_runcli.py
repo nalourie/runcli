f449a02578d8b4f27a19954a9b18d032a78b3cb4
Nicholas Lourie <nalourie@gmail.com>
2017-07-16 23:18:24 -0700

    Move TemporaryDirectory into its own utils module
    Move TemporaryDirectory into its own utils module since I'd like to
    use it in the project wide test suite that will be written, and it
    would look strange to import something from a test file in another
    module.

--- runcli/test_runcli.py
+++ runcli/test_runcli.py
@@ -6,39 +6,14 @@ Tests for ``runcli.runcli``.
 
 import logging
 import os
-import shutil
 import stat
-import tempfile
 import unittest
 
 from . import runcli
+from . import utils
 
 
 logger = logging.getLogger(__name__)
-
-
-# eventually this context manager should be replaced with
-# tempfile.TemporaryDirectory when only newer versions of python are
-# targeted by this project.
-class TemporaryDirectory(object):
-    """A context manager that creates a temporary directory.
-
-    This context manager returns the path to a temporary directory which
-    is destroyed upon exiting the context.
-
-    Use this context manager for python 2.7 compatibility. A version of
-    this context manager exists in the tempfile module in newer versions
-    of python.
-    """
-
-    def __enter__(self):
-        """Create an return a temporary directory."""
-        self.tmp_dir_path = tempfile.mkdtemp()
-        return self.tmp_dir_path
-
-    def __exit__(self, *args):
-        """Remove the temporary directory created by __enter__."""
-        shutil.rmtree(self.tmp_dir_path)
 
 
 class TestGetRunfilePath(unittest.TestCase):
@@ -50,9 +25,9 @@ class TestGetRunfilePath(unittest.TestCase):
         Test that ``get_runfile_path` gets the correct path to the
         Runfile when in the same directory as the Runfile.
         """
-        with TemporaryDirectory() as tmp_dir_path:
+        with utils.TemporaryDirectory() as tmp_dir_path:
             # on MacOS '/var' is a symlink to '/private/var' so we need
-            # to get the real path returned by TemporaryDirectory
+            # to get the real path returned by utils.TemporaryDirectory
             tmp_dir_path = os.path.realpath(tmp_dir_path)
             runfile_path = os.path.join(tmp_dir_path, 'Runfile')
             with open(runfile_path, 'w') as runfile:
@@ -70,9 +45,9 @@ class TestGetRunfilePath(unittest.TestCase):
         Runfile when in a subdirectory of the directory contianing the
         Runfile.
         """
-        with TemporaryDirectory() as tmp_dir_path:
+        with utils.TemporaryDirectory() as tmp_dir_path:
             # on MacOS '/var' is a symlink to '/private/var' so we need
-            # to get the real path returned by TemporaryDirectory
+            # to get the real path returned by utils.TemporaryDirectory
             tmp_dir_path = os.path.realpath(tmp_dir_path)
             runfile_path = os.path.join(tmp_dir_path, 'Runfile')
             with open(runfile_path, 'w') as runfile:
@@ -87,9 +62,9 @@ class TestGetRunfilePath(unittest.TestCase):
 
     def test_raises_error_when_runfile_missing(self):
         """Test ``get_runfile_path`` raises error if Runfile is missing."""
-        with TemporaryDirectory() as tmp_dir_path:
+        with utils.TemporaryDirectory() as tmp_dir_path:
             # on MacOS '/var' is a symlink to '/private/var' so we need
-            # to get the real path returned by TemporaryDirectory
+            # to get the real path returned by utils.TemporaryDirectory
             tmp_dir_path = os.path.realpath(tmp_dir_path)
 
             os.chdir(tmp_dir_path)
@@ -102,9 +77,9 @@ class TestMain(unittest.TestCase):
 
     def test_main(self):
         """Test ``runcli.main`` end to end."""
-        with TemporaryDirectory() as tmp_dir_path:
+        with utils.TemporaryDirectory() as tmp_dir_path:
             # on MacOS '/var' is a symlink to '/private/var' so we need
-            # to get the real path returned by TemporaryDirectory
+            # to get the real path returned by utils.TemporaryDirectory
             tmp_dir_path = os.path.realpath(tmp_dir_path)
             runfile_path = os.path.join(tmp_dir_path, 'Runfile')
             tmp_output_path = os.path.join(tmp_dir_path, 'tmp.txt')
